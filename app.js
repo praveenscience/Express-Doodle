18e96c4f37916eedff2d75dbfcf9a93d27b2b5f3
Praveen Kumar <praveenscience@gmail.com>
2020-11-21 12:29:57 +0000

    Updated the code to use HTTP server for both IO & Static.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.use("/", express.static(__dirname + "/public"));
 
-app.listen(3000, () => {
+http.listen(3000, () => {
   console.log("Server started in Port 3000.");
 });
