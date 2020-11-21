9a80179ee83b08632ff53da5334e75fc3bd6a4de
Praveen Kumar <praveenscience@gmail.com>
2020-11-21 12:33:40 +0000

    Added a broadcast server.

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const io = require("socket.io")(http);
 
 app.use("/", express.static(__dirname + "/public"));
 
+const onConnection = socket => {
+  socket.on("drawing", data => socket.broadcast.emit("drawing", data));
+};
+
+io.on("connection", onConnection);
+
 http.listen(3000, () => {
   console.log("Server started in Port 3000.");
 });
