697bb9eb8dc052900ebfc1ab54596958c156cbf7
Praveen Kumar <praveenscience@gmail.com>
2020-11-21 12:21:14 +0000

    Setup an Express JS server.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello Everyone!");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("Hello, Everyone!");
+});
+
+app.listen(3000, () => {
+  console.log("Server started in Port 3000.");
+});
